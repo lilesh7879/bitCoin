7cbdf57e809c882edcb01d0764bf36eb7beb6702
lilesh7879 <luckyshrivas313@gmail.com>
2021-03-18 22:11:36 +0530

    changes CSS

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Currency from './Currency';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import './styles.css'
 
 function App() {
 
-    return <div>
+    return <div className="parentContainer">
 
         <br />
         <Currency />
